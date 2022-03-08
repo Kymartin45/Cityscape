97458dd71bc69653cffc2f90337db3d691d55758
Kyle Martin <kymartin4505@gmail.com>
2022-03-07 17:42:44 -0800

    Db query to filter cities by population

--- db/connectDb.py
+++ db/connectDb.py
@@ -1,3 +1,4 @@
+from main import POPULATION_THRESHHOLD
 from dotenv import dotenv_values
 import psycopg2
 
@@ -5,21 +6,25 @@ config = dotenv_values('.env')
 POSTGRESQL_PASSWORD = config.get('POSTGRESQL_PASSWORD')
 POSTGRES_HOST = config.get('POSTGRES_HOST')
 
-def connectToDb():
-    try:
-        conn = psycopg2.connect(
-            host= POSTGRES_HOST,
-            database='postgres',
-            user='postgres',
-            password=POSTGRESQL_PASSWORD
-        )
-        print('Connection established with DB')
-        conn.close()
-        return True 
-    except: 
-        return False
+CONN = psycopg2.connect(
+    host=POSTGRES_HOST,
+    database='postgres',
+    user='postgres',
+    password=POSTGRESQL_PASSWORD
+)
+
+def filteredCitiesByPopulation(populationThreshold):
+    cur = CONN.cursor()
+    cur.execute(''' 
+                    SELECT city, population FROM worldwidecities
+                    WHERE population >= %s
+                    ORDER BY population DESC; ''', [populationThreshold])
+    filteredCities = list(cur.fetchall())
+    CONN.commit()
+    CONN.close()
+    return filteredCities
 
 if __name__ == '__main__':
-    connectToDb()
+    filteredCitiesByPopulation(POPULATION_THRESHHOLD)
 
     
