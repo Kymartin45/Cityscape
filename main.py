72c105a98d2e934d43548687c77847458ff55262
Kyle Martin <kymartin4505@gmail.com>
2022-03-08 15:13:28 -0800

    Get user guess from form & declare country in City class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import dotenv_values
 from db import connectDb
-from random import choice
 import urllib.parse
 import requests
 import json
@@ -16,14 +15,15 @@ SECRET_KEY = config.get('SECRET_KEY')
 POPULATION_THRESHHOLD = 1000000
 
 class City:
-    def __init__(self, name, coords):
+    def __init__(self, name, coords, country):
         self.name = name 
         self.coords = coords 
+        self.country = country
 
 def getRandomCityName(populationThreshold):
-    filteredCities = connectDb.filteredCitiesByPopulation(populationThreshold)
-    city = choice(filteredCities)
-    return city
+    city = connectDb.getCitiesByPopulation(populationThreshold)
+
+    return city[0]
 
 def getCityCoordinatesByName(cityName):
     geoUrl = 'https://api.opencagedata.com/geocode/v1/json'
@@ -40,10 +40,10 @@ def getCityCoordinatesByName(cityName):
     return (lon, lat)
 
 def getRandomCity():
-    cityName = getRandomCityName(POPULATION_THRESHHOLD)
+    cityName, cityCountry = getRandomCityName(POPULATION_THRESHHOLD)
     cityCoords = getCityCoordinatesByName(cityName)
-    city = City(cityName, cityCoords)
-    print(f'{city.name} : {city.coords}')
+    city = City(cityName, cityCoords, cityCountry)
+    print(f'{city.name}, {city.country} : {city.coords}')
     
     return city 
 
@@ -52,10 +52,11 @@ def showMap(city):
     url = 'https://maps.geoapify.com/v1/staticmap?'
     params = {
         'style': 'osm-bright',
-        'width': '1920',  
-        'height': '1080',
+        'scaleFactor': 2,
+        'width': '800',  
+        'height': '600',
         'center': f'lonlat:{lon},{lat}',
-        'zoom': '6.5',
+        'zoom': '6',
         'marker': f'lonlat:{lon},{lat};color:#ff0000;size:medium',
         'apiKey': GEOAPIFY_API_KEY
     }    
@@ -63,12 +64,19 @@ def showMap(city):
     
     return fullUrl
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     city = getRandomCity()
     mapOfCity = showMap(city)
+
     return render_template('index.html', mapOfCity=mapOfCity)
-    
+
+@app.route('/', methods=['GET', 'POST'])
+def checkGuess(userGuess=None):
+    userGuess = request.form['submit-guess'].title()
+
+    return userGuess
+
 if __name__ == '__main__':
     app.secret_key = SECRET_KEY
     app.run(debug=True, port=8000)
