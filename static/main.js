e4efbf985606027299e76da2a7e3627ee4f1c451
Kyle Martin <kymartin4505@gmail.com>
2022-04-05 13:13:52 -0700

    Adj made to leaderboard. Temp method to show all stats via modal

--- static/main.js
+++ static/main.js
@@ -1,16 +1,31 @@
-window.onload = function refreshInputFields() {
-    document.getElementById('user-submit-guess').addEventListener('click', submitGuess, {once: true});
-
-    if (!localStorage.getItem('currentStreak') && !localStorage.getItem('maxStreak')) {
-        localStorage.setItem('currentStreak', '0');
-        localStorage.setItem('maxStreak', '0');
-        localStorage.setItem('gamesPlayed', '0');
-        localStorage.setItem('gamesWon', '0');
-        localStorage.setItem('winPercentage', '0');
-    }
-}
-
 function refreshPage() {
     window.location.reload();
+}
+
+function showGlobalLeaderboard() {
+    fetch('/leaderboard')
+    .then((res) => res.json())
+    .then(globalLeaderboard => {
+        const gamesPlayed = document.getElementById('games-played');
+        const gamesWon = document.getElementById('games-won');
+        const leaderboard = globalLeaderboard['leaderboard'];
+        console.log(globalLeaderboard);
+                
+        document.getElementById('total-player-count').textContent = globalLeaderboard['totalPlayers'];
+
+        // for every player in db, list out their stats (gamesPlayed, gamesWon)
+        // continues to append new `p` element, so gotta find workaround to display all stats
+        leaderboard.forEach((item) => {
+            const pGamesPlayed = document.createElement('p');
+            const pGamesWon = document.createElement('p');
+
+            pGamesPlayed.textContent = item['gamesPlayed'];
+            pGamesWon.textContent = item['gamesWon'];
+
+            gamesPlayed.append(pGamesPlayed);
+            gamesWon.append(pGamesWon);
+        })
+    })
+    .catch((err) => { console.error(err); })
 }
 
